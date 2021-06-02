19744e0cc95d2f2da7afdf3db765d6a8c8d536e5
André Kunde <andre.kunde@zup.com.br>
2021-06-02 11:01:50 -0300

    migrations and create / list transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,29 @@
 // import AppError from '../errors/AppError';
 
-import Transaction from '../models/Transaction';
+import { getCustomRepository } from 'typeorm';
 
+import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
